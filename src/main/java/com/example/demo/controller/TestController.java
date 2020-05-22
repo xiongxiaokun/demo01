1819b3b013bd04e12364ac37649e688c07f426a5
xiongxk <983210575@qq.com>
2020-05-22 16:08:02 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello () {
+        return "hello world!";
+    }
+
+}
